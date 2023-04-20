f3956b448394c02aafa2869077b527f9bc07e5c5
kaiocesarnp <kaiocesarnp@gmail.com>
2023-04-20 09:47:11 -0300

    songtext

--- songText.py
+++ songText.py
@@ -34,6 +34,7 @@ except sr.ResquestError as e:
 # text = r.recognize_google(audio, language='pt-BR'): Tenta reconhecer o áudio gravado usando o serviço de reconhecimento de fala do Google (recognize_google) fornecido pela biblioteca SpeechRecognition. O resultado do reconhecimento é armazenado na variável text.
 
 # print(f"Você disse: {text}"): Imprime o texto reconhecido na linha anterior na tela.
+    # O "f" indica que é uma string formatada e as chaves {} são usadas para incluir as variáveis ou expressões dentro da mensagem. Quando a mensagem é exibida, o valor da variável "text" é inserido no local onde as chaves estão colocadas.
 
 # except sr.UnknownValueError:: Se ocorrer um erro de reconhecimento de voz, como não ser possível entender o que foi dito, este bloco except será executado e imprimirá uma mensagem de erro.
 
