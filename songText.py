4bb5bcadf600467a32ad3848e1fa57d832a3185c
kaiocesarnp <kaiocesarnp@gmail.com>
2023-04-20 09:28:10 -0300

    songtext

--- songText.py
+++ songText.py
@@ -15,3 +15,22 @@ except sr.UnknowValueError:
     print("Não entendi o que você disse!")
 except sr.ResquestError as e:
     print("Erro de requisição; {0}".format(e))
+
+
+# Explicação de cada linha:
+
+# import speech_recognition as sr: importa a biblioteca SpeechRecognition e a renomeia para sr, para facilitar a referência;
+
+# r = sr.Recognizer(): Cria uma nova instância da classe Recognizer da biblioteca SpeechRecognition, que é usada para reconhecer o áudio capturado.
+
+
+
+
+
+
+
+
+
+
+
+
